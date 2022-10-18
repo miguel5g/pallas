53fbe400787c2aafc4c077b328a657481f9babe0
Miguel Ângelo <miguelcg958@gmail.com>
2022-10-18 18:19:32 -0300

    tests: add get all users controller test cases

--- src/controllers/get-all-users.controller.spec.js
+++ src/controllers/get-all-users.controller.spec.js
@@ -1,18 +1,76 @@
-import { describe, expect, it, beforeEach } from '@jest/globals';
+import { afterEach, beforeAll, beforeEach, describe, expect, it, jest } from '@jest/globals';
 
 import { GetAllUsersController } from './get-all-users.controller';
+import { GetAllUsersService } from '../services/get-all-users.service';
 
 describe('GetAllUsersController.js', () => {
   /** @type {GetAllUsersController} */
   let controller;
+  /** @type {import('express').Request} */
+  let request;
+  /** @type {import('express').Response} */
+  let response;
+
+  beforeAll(() => {
+    GetAllUsersService.prototype.handler = jest.fn();
+  });
 
   beforeEach(() => {
-    controller = new GetAllUsersController();
+    controller = new GetAllUsersController(new GetAllUsersService());
+
+    request = { query: {} };
+    response = { json: jest.fn() };
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should have a handler method', () => {
     expect(controller.handler).toBeDefined();
   });
 
-  it.todo("should calls response.json with 'Under construction' message");
+  it('should calls service.handler', async () => {
+    await controller.handler(request, response);
+
+    expect(GetAllUsersService.prototype.handler).toBeCalledTimes(1);
+  });
+
+  it('should return a json body with pagination', async () => {
+    request.query.page = '15';
+    const expectedOutput = {
+      page: 15,
+      limit: 20,
+      users: ['user 1', 'user 2', 'user 3'],
+    };
+
+    GetAllUsersService.prototype.handler.mockResolvedValueOnce(expectedOutput);
+
+    await controller.handler(request, response);
+
+    expect(GetAllUsersService.prototype.handler).toBeCalledWith(15);
+    expect(response.json).toBeCalledTimes(1);
+    expect(response.json).toBeCalledWith(expectedOutput);
+  });
+
+  it('should return pagination fields when multiple page number has been passed', async () => {
+    request.query.page = ['30', '15'];
+    const expectedOutput = {
+      page: 30,
+      limit: 20,
+      users: ['user 1', 'user 2', 'user 3'],
+    };
+
+    GetAllUsersService.prototype.handler.mockResolvedValueOnce(expectedOutput);
+
+    await controller.handler(request, response);
+
+    expect(GetAllUsersService.prototype.handler).toBeCalledWith(30);
+    expect(response.json).toBeCalledTimes(1);
+    expect(response.json).toBeCalledWith(expectedOutput);
+  });
+
+  it('should throw an error if not pass service to controller constructor', () => {
+    expect(() => new GetAllUsersController()).toThrow('Invalid service instance');
+  });
 });
