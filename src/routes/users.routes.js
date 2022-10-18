a54a5f29e26534876815ad3b3765128f47bd4e76
Miguel Ângelo <miguelcg958@gmail.com>
2022-10-18 18:21:42 -0300

    fix: pass controller service to controller instance

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
 import { GetAllUsersController } from '../controllers/get-all-users.controller';
+import { GetAllUsersService } from '../services/get-all-users.service';
 
 const usersRoutes = Router();
-const getAllUsersController = new GetAllUsersController();
+const getAllUsersController = new GetAllUsersController(new GetAllUsersService());
 
 usersRoutes.get('/', getAllUsersController.handler);
 
