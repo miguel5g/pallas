3056ebf2cd2eab45b0d6b90e00898cca1412e790
Miguel Ângelo <miguelcg958@gmail.com>
2022-10-18 17:18:03 -0300

    feat: implement get all users service

--- src/services/get-all-users.service.js
+++ src/services/get-all-users.service.js
@@ -3,8 +3,20 @@ import { prisma } from '../libs/prisma';
 class GetAllUsersService {
   constructor() {}
 
-  async handler() {
-    prisma.user.findMany();
+  async handler(page) {
+    const validPage = Math.trunc(Math.max(1, +page)) || 1;
+
+    return prisma.user.findMany({
+      skip: (validPage - 1) * 20,
+      take: 20,
+      select: {
+        id: true,
+        name: true,
+        surname: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
   }
 }
 
