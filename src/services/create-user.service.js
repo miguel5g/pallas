477a74b09b336ab5c05e066eaeb4bd73780394e6
Miguel Ângelo <miguelcg958@gmail.com>
2022-10-20 14:45:40 -0300

    feat: add password encryption

--- src/services/create-user.service.js
+++ src/services/create-user.service.js
@@ -1,9 +1,12 @@
+import { hashText } from '../libs/encryption';
 import { prisma } from '../libs/prisma';
 
 class CreateUserService {
   async handler({ name, surname, email, password }) {
+    const hashedPassword = await hashText(password);
+
     await prisma.user.create({
-      data: { name, surname, email, password },
+      data: { name, surname, email, password: hashedPassword },
     });
   }
 }
