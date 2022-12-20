5bc2dd8226a5be4a4222b946d25625ea01e80a88
Miguel Ângelo <miguelcg958@gmail.com>
2022-12-20 17:05:55 -0300

    tests(prisma mock): add more mocked methods

--- src/tests/helpers/prisma-mock.js
+++ src/tests/helpers/prisma-mock.js
@@ -14,6 +14,7 @@ jest.mock('../../libs/prisma', () => {
         count: jest.fn(),
         create: jest.fn(),
         findMany: jest.fn(),
+        findFirst: jest.fn(),
         findUnique: jest.fn(),
         update: jest.fn(),
       },
