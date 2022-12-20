f911b6fc4be41a32029bacade77ec7fccce2ce80
Miguel Ângelo <miguelcg958@gmail.com>
2022-12-20 17:16:20 -0300

    feat: implement update task endpoint

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -3,20 +3,24 @@ import { Router } from 'express';
 import { CreateTaskController } from '../controllers/tasks/create-task.controller';
 import { GetAllTasksController } from '../controllers/tasks/get-all-tasks.controller';
 import { GetTaskByIdController } from '../controllers/tasks/get-task-by-id.controller';
+import { UpdateTaskController } from '../controllers/tasks/update-task.controller';
 import { ProtectedRouteMiddleware } from '../middlewares/protected-route.middleware';
 import { CreateTaskService } from '../services/tasks/create-task.service';
 import { GetAllTasksService } from '../services/tasks/get-all-tasks.service';
 import { GetTaskByIdService } from '../services/tasks/get-task-by-id.service';
+import { UpdateTaskService } from '../services/tasks/update-task.service';
 
 const taskRoutes = Router();
 
 const createTaskController = new CreateTaskController(new CreateTaskService());
 const getAllTasksController = new GetAllTasksController(new GetAllTasksService());
 const getTaskByIdController = new GetTaskByIdController(new GetTaskByIdService());
+const updateTaskController = new UpdateTaskController(new UpdateTaskService());
 const protectedRouteMiddleware = new ProtectedRouteMiddleware();
 
 taskRoutes.post('/', protectedRouteMiddleware.handler, createTaskController.handler);
 taskRoutes.get('/', protectedRouteMiddleware.handler, getAllTasksController.handler);
 taskRoutes.get('/:id', protectedRouteMiddleware.handler, getTaskByIdController.handler);
+taskRoutes.patch('/:id', protectedRouteMiddleware.handler, updateTaskController.handler);
 
 export { taskRoutes };
