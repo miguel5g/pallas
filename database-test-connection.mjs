e64be7924305686a9a962d888086a391bfc38993
Miguel Ângelo <miguelcg958@gmail.com>
2022-12-21 15:55:05 -0300

    chore: exit with error if unable to connect to db

--- database-test-connection.mjs
+++ database-test-connection.mjs
@@ -2,13 +2,21 @@ import retry from 'retry';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+const MAX_TRIES = 10;
 
 function connect() {
   const connection = retry.operation({
     factor: 3,
-    retries: 10,
+    retries: MAX_TRIES,
     maxTimeout: 30 * 1000,
   });
+
+  function exitWithErro() {
+    if (connection.attempts() === MAX_TRIES + 1) {
+      console.log('Unable to connect to database');
+      process.exit(1);
+    }
+  }
 
   connection.attempt((currentAttempt) => {
     console.log(`Trying to connect to database (attempt ${currentAttempt})`);
@@ -16,7 +24,10 @@ function connect() {
     prisma
       .$connect()
       .then(() => console.log('Successfully connected'))
-      .catch((error) => connection.retry(error));
+      .catch((error) => {
+        exitWithErro();
+        connection.retry(error);
+      });
   });
 }
 
